55ff8f674a0946f7f84d2b92f19bea641e97f159
Hicham DAKHOUCH <dakhouchch047@gmail.com>
2024-11-21 11:30:57 +0100

    modification 4.3

--- api/addMessages.js
+++ api/addMessages.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
     res.status(200).json({ success: true });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ error: "Internal Server Error", details: error.message });
   }
 }
 
